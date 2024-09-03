706bd65037159f06e19b71ea57b86bc372e534c9
ChanakaDev <mr.mbcnb@gmail.com>
2024-09-03 14:38:04 +0530

    Implemented an intent for loading the sign in screen after the splash screen

--- app/src/main/java/com/example/todoapp/SplashActivity.java
+++ app/src/main/java/com/example/todoapp/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.todoapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +24,16 @@ public class SplashActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Load LoginActivity after a delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start LoginActivity (or MainActivity if user is already logged in)
+                Intent intent = new Intent(SplashActivity.this, SigninActivity.class);
+                startActivity(intent);
+                finish(); // Close SplashActivity
+            }
+        }, 3000); // Delay for 3 seconds
     }
 }
