1ee144736506d309e629fffdae09e1cc27b80d20
ChanakaDev <mr.mbcnb@gmail.com>
2024-09-03 15:48:29 +0530

    Implemented the sign in to todo screen navogation

--- app/src/main/java/com/example/todoapp/SigninActivity.java
+++ app/src/main/java/com/example/todoapp/SigninActivity.java
@@ -1,6 +1,9 @@
 package com.example.todoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class SigninActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button signUpBtn = findViewById(R.id.signin_button);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SigninActivity.this, TodoActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
