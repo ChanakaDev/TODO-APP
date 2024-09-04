1c9a8b347f6b640be55c067dbacd4eaa1ecfe895
ChanakaDev <mr.mbcnb@gmail.com>
2024-09-04 14:31:12 +0530

    Implemented the sign in to sign up screen navigation

--- app/src/main/java/com/example/todoapp/SigninActivity.java
+++ app/src/main/java/com/example/todoapp/SigninActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +31,19 @@ public class SigninActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SigninActivity.this, TodoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Find the TextView by ID
+        TextView signupText = findViewById(R.id.signup_text);
+
+        // Set an OnClickListener on the TextView
+        signupText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to SignUpActivity
+                Intent intent = new Intent(SigninActivity.this, SignupActivity.class);
                 startActivity(intent);
             }
         });
