1d70f4b6423d60a172072dbc74a7bf44451b3f6e
ChanakaDev <mr.mbcnb@gmail.com>
2024-09-04 14:56:11 +0530

    Implemented navigation from todo screen to dev info screen

--- app/src/main/java/com/example/todoapp/TodoActivity.java
+++ app/src/main/java/com/example/todoapp/TodoActivity.java
@@ -1,6 +1,10 @@
 package com.example.todoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,17 @@ public class TodoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageButton menuIcon = findViewById(R.id.menuIcon);
+        ImageButton devInfoIcon = findViewById(R.id.devInfoIcon);
+
+        devInfoIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TodoActivity.this, DevinfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
